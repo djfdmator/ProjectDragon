6b5c119e577fc907df4b61b3208bf0330aa98597
djfdmator <djfdmator@naver.com>
2020-11-28 01:30:14 +0900

    release version 108

--- ProjectDragon/Assets/Scripts/Manager/GameManager.cs
+++ ProjectDragon/Assets/Scripts/Manager/GameManager.cs
@@ -37,7 +37,7 @@ public class GameManager : MonoSingleton<GameManager>
     public IDbCommand DEB_dbcmd;
     public bool loadComplete = false;
 
-    private string DBName = "/DS_Database_vr_107.sqlite";
+    private string DBName = "/DS_Database_vr_108.sqlite";
 
     private void Awake()
     {
