3f0a3cb5c8e46118607f634f142f41d546846e6d
tpdms025 <45121963+tpdms025@users.noreply.github.com>
2020-12-21 17:06:47 +0900

    Revert "backup"
    This reverts commit 7c9c3228ca345a6beb3544bc607b602caa1e713d.

--- ProjectDragon/Assets/Scripts/A_Star/t_Node.cs
+++ ProjectDragon/Assets/Scripts/A_Star/t_Node.cs
@@ -1,12 +1,4 @@
-﻿// ==============================================================
-// A*에 사용될 Node
-//
-//  AUTHOR: Yang SeEun
-// CREATED:
-// UPDATED: 2020-01-01
-// ==============================================================
-
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
