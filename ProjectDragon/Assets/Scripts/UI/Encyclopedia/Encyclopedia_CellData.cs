a4c104886ca039c28112c97b6581a2f5f9f6b91a
tpdms025 <seyang.dev@gmail.com>
2020-12-27 15:31:18 +0900

    도감,업적 이미지와 UI 추가 & DB 변경 & 겜매니저 수정

---
+++ ProjectDragon/Assets/Scripts/UI/Encyclopedia/Encyclopedia_CellData.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Encyclopedia_CellData : MonoBehaviour
+{
+    private GameObject deactivationObj;
+    private GameObject activationObj;
+
+    public UISprite deactivationSprite;
+    public UISprite activationSprite;
+    public UIButton button;
+
+    public int DB_Num = -1;
+    private string spriteName = "Encyclopedia_Slot_";
+
+    private void Awake()
+    {
+        deactivationObj = transform.Find("Deactivation").gameObject;
+        activationObj = transform.Find("Activation").gameObject;
+
+        deactivationSprite = deactivationObj.GetComponent<UISprite>();
+        activationSprite = activationObj.GetComponent<UISprite>();
+        button = activationObj.GetComponent<UIButton>();
+
+    }
+
+    //초기화
+    public void Init(Database.Encyclopedia encyclopedia)
+    {
+        DB_Num = encyclopedia.num;
+
+        deactivationSprite.spriteName = spriteName + encyclopedia.imageName + "_Gray";
+        activationSprite.spriteName = spriteName + encyclopedia.imageName;
+        button.normalSprite = spriteName + encyclopedia.imageName;
+
+        ToggleActive(encyclopedia.active);
+    }
+    public void Init(Database.Achievement achievement)
+    {
+        DB_Num = achievement.num;
+
+        deactivationSprite.spriteName = spriteName + achievement.imageName + "_Gray";
+        activationSprite.spriteName = spriteName + achievement.imageName;
+        button.normalSprite = spriteName + achievement.imageName;
+
+        ToggleActive(achievement.active);
+    }
+
+
+    public void ToggleActive(bool activation)
+    {
+        activationObj.SetActive(activation);
+        deactivationObj.SetActive(!activation);
+    }
+   
+
+}
